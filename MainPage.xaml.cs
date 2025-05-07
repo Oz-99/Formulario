fbecf96b2fdc7037e814e9f2d6ba18cfea7a0b8a
Oz-99 <alheja1997@gmail.com>
2025-05-07 13:04:33 -0500

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace Formulario
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnRegistrarClicked(object sender, EventArgs e)
+        {
+            string nombre = nombreEntry.Text;
+            string email = emailEntry.Text;
+            string telefono = telefonoEntry.Text;
+            string genero = generoPicker.SelectedItem?.ToString() ?? "Genero no Seleccionado";
+
+            if (string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(email) || string.IsNullOrEmpty(telefono))
+            {
+                await DisplayAlert("Error", "Llena todos los campos!", "OK");
+                return;
+            }
+            await DisplayAlert("Formulario Diligenciado con Exito!", $"Nombre: {nombre}\nEmail: {email}\nTelefono: {telefono}\nGenero: {genero}", "OK");
+        }
+
+
+    }
+
+}
